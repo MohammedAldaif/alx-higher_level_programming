5ac18e3f0302c6ee56a3e581eb2dbeac0d2dc68c
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-01-04 16:39:12 -0800

    m

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+    def width(self):
+        return self.__width
+    def height(self):
+        return self.__height
+    def width(self, value):
+        self.__width = value
+    def height(self, value):
+        if type(value) is not int:
+            raise typeError("height must be an integer");
+        if value < 0:
+            raise valueError("height must be >= 0")
+        self.__height = value
