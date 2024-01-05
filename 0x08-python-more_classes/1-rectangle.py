0bd5c508632224ba2e7ab6f1e9646e676611bdec
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-01-04 16:48:19 -0800

    m

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 
+"""Create a class Rectangle that defines a rectangle"""
+
 class Rectangle:
+    """rectangle class"""
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
     def width(self):
+        """retreive attribute"""
         return self.__width
     def height(self):
         return self.__height
     def width(self, value):
         self.__width = value
     def height(self, value):
+        """to set the attribute"""
         if type(value) is not int:
             raise typeError("height must be an integer");
         if value < 0:
