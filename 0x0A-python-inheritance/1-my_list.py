c193a9d533052f85701ace8a4acebe1d258b0fe8
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-01-10 16:50:23 -0800

    m

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
 """description"""
 
 class MyList(list):
     """class"""
     def print_sorted(self):
         """description"""
-
         sorted_list = sorted(self)
         print(sorted_list)
-
