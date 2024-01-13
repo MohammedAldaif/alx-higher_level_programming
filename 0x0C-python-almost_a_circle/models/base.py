bccbaa59dc3b66f523418753380e9008059b94d9
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-01-12 16:33:46 -0800

    M

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,13 +3,15 @@
 
 
 class Base:
-    """class"""
+    """tests"""
+
     __nb_objects = 0
 
-    def __init__(self, id=None):
-        """constructor"""
+    def __init__(self,id=None):
+        """tests"""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""rectangle"""
+"""test"""
 from models.base import Base
 
 
-class Rectangle(base):
-    """class"""
-
+class Rectangle(Base):
+    """test"""
     def __init__(self,width,height,x=0,y=0,id=None):
-        """constructor"""
+        """test"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -15,29 +14,51 @@ class Rectangle(base):
         self.y = y
     @property
     def width(self):
-        """width of the class"""
+        """widht of rectangle"""
         return self.__width
     @width.setter
     def width(self,value):
-        self.__width=value
+        self.validate_integer("width",value,False)
+        self.__width = value
     @property
     def height(self):
-        """hegiht of the class"""
+        """widht of rectangle"""
         return self.__height
     @height.setter
     def height(self,value):
-        self.height = value
+        self.validate_integer("height",value,False)
+        self.__height = value
     @property
     def x(self):
-        """x of this class"""
+        """widht of rectangle"""
         return self.__x
     @x.setter
     def x(self,value):
+        self.validate_integer("x",value)
         self.__x = value
     @property
     def y(self):
-        """y of this class"""
+        """widht of rectangle"""
         return self.__y
     @y.setter
     def y(self,value):
+        self.validate_integer("y",value)
         self.__y = value
+    def validate_integer(self,name,value,eq=True): 
+        """method to validate errors"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not eq and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
+    def area(self):
+        """calculate area for the rectangle"""
+        return self.width * self.height
+    def display(self):
+        """print rectangle area by # samples"""
+        s = '\n' * self.y + (' ' * self.x + '#' * self.width + '\n') * self.height
+        print(s,end = '')
+    def __str__(self):
+        """Test"""
+        return '[{}] ({}) {}/{} - {}/{}'.format(type(self).__name__,self.id, self.x, self.y,self.width,self.height)
