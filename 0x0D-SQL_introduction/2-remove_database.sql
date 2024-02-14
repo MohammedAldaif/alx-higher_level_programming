078837cfbf6b095b73dd18d9cf398a14a4e178c3
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-02-13 23:55:27 -0800

    m

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,5 @@
---drop command
+-- deletes the database hbtn_0c_0 from my MySQL server
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- not allowed to use SELECT or SHOW statements
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
