6dd6f12d49a53bf1a88bc091165b333c4cbd6a82
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-01 22:57:56 -0800

    M

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,18 +5,16 @@ import random
 number = random.randint(-10000, 10000)
 
 # Get the last digit of the number
-num = abs(number) % 10
-last_digit = num if number >= 0 else num * -1
+last_digit = number % 10
 result = "last digit of {} is {}".format(number, last_digit)
 
-# Use match statement for pattern matching
-match last_digit:
-    case _ if last_digit > 5:
-        result += " and is greater than 5"
-    case 0:
-        result += " and is 0"
-    case _ if 0 < last_digit < 6:
-        result += " and is less than 6 and not 0"
+# Check conditions and append to result string
+if last_digit > 5:
+    result += " and is greater than 5"
+elif last_digit == 0:
+    result += " and is 0"
+else:
+    result += " and is less than 6 and not 0"
 
 # Print the result
 print(result)
