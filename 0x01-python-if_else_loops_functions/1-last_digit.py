23168fda649b9d1d4676012eeffbe8039a1995fc
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-01 23:00:50 -0800

    M

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,17 +4,17 @@ import random
 # This line should not change
 number = random.randint(-10000, 10000)
 
-# Get the last digit of the number
+# Get the last digit of the number, preserving the sign
 last_digit = number % 10
-result = "last digit of {} is {}".format(number, last_digit)
+result = "Last digit of {} is {} and is".format(number, last_digit)
 
 # Check conditions and append to result string
 if last_digit > 5:
-    result += " and is greater than 5"
+    result += " greater than 5"
 elif last_digit == 0:
-    result += " and is 0"
+    result += " 0"
 else:
-    result += " and is less than 6 and not 0"
+    result += " less than 6 and not 0"
 
 # Print the result
 print(result)
