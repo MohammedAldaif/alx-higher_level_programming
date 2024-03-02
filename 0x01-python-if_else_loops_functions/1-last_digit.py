7fc8886c3d1c0c32984fa36c7a46b49dfa66aa4a
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-01 22:46:42 -0800

    M

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 import random
+
+# This line should not change
 number = random.randint(-10000, 10000)
-last_dgt = abs(number) % 10
-if number < 0:
-    last_dgt = -last_dgt
-    
-if last_dgt > 5:
-    print(f"Last digit of {number} is {last_dgt} and is greater than 5")
-elif last_dgt < 6 and last_dgt != 0:
-    print(f"Last digit of {number} is {last_dgt} and is less than 6 and not 0")
-else:
-    print(f"Last digit of {number} is {last_dgt} and is 0")
+
+# Get the last digit of the number
+num = abs(number) % 10
+last_digit = num if number >= 0 else num * -1
+result = "last digit of {} is {}".format(number, last_digit)
+
+# Use match statement for pattern matching
+match last_digit:
+    case _ if last_digit > 5:
+        result += " and is greater than 5"
+    case 0:
+        result += " and is 0"
+    case _ if 0 < last_digit < 6:
+        result += " and is less than 6 and not 0"
+
+# Print the result
+print(result)
