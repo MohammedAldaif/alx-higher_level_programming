c2814ababc5a227a513e71213c3646a48304d8a8
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-11 09:30:56 -0700

    MM

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const input = process.argv[2];
+const number = parseInt(input, 10);
+
+if (!isNaN(number)) {
+  console.log(`My number: ${number}`);
+} else {
+  console.log('Not a number');
+}
