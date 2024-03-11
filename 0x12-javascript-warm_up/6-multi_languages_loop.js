e68992298cd45bf7995fb63ed01a4d459fdb3e48
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-11 09:40:58 -0700

    m

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-lines = ['C is fun','Python is cool','JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < lines.length) {
-	console.log(lines[i]);
-	i++;
+  console.log(lines[i]);
+  i++;
 }
