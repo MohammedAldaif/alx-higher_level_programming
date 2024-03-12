16ea2e6f88448a1ab0819a43943e1c3060b69b49
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-11 18:04:24 -0700

    M

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let a = parseInt(process.argv[2],10);
-let b = parseInt(process.argv[3],10);
-function add(a,b){
-	return a + b;
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+function add (a, b) {
+  return a + b;
 }
-console.log(add(a,b));
+console.log(add(a, b));
