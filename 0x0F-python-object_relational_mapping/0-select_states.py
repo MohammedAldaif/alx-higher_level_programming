19023e6f727445f2d70edc16e4fa4f39ad63eb7e
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-14 10:37:26 -0700

    MM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,6 @@ Script that lists all states from the database hbtn_0e_0_usa.
 import sys
 import MySQLdb
 
-
 def list_states(username, password, database):
     """
     Lists all states from the database hbtn_0e_0_usa.
@@ -29,8 +28,8 @@ def list_states(username, password, database):
         # Create cursor object
         cursor = db.cursor()
 
-        # Execute SQL query
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        # Execute SQL query to fetch distinct states
+        cursor.execute("SELECT DISTINCT id, name FROM states ORDER BY id ASC")
 
         # Fetch all rows
         rows = cursor.fetchall()
@@ -49,7 +48,10 @@ def list_states(username, password, database):
         if db:
             db.close()
 
-
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
     username, password, database = sys.argv[1:]
     list_states(username, password, database)
