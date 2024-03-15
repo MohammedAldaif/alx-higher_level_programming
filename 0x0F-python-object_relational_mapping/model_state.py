4c8d0009b24b7a4742a75b918dedb3d48494da43
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-15 10:29:12 -0700

    M

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,4 +1,6 @@
-"""Defines the State class and creates the Base instance."""
+"""Defines the State class and creates the Base instance
+"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -6,7 +8,20 @@ Base = declarative_base()
 
 
 class State(Base):
-    """State class to link with the states table in the database."""
+    """Represents a state in the database.
+
+    This class represents a state in the database,links to the 'states' table
+    and defines two columns: 'id' and 'name'.
+
+    Attributes:
+        id (int): The unique id of the state.
+        name (str): The name of the state.
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    # This code will only run when the script is executed directly
+    pass  # You can add any additional code to be executed here if needed
