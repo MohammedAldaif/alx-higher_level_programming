09f5fde9fa9328f71d1679c5dc07b73e08e247a1
MohammedAldaif <mohammedaldaif71@gmail.com@example.com>
2024-03-17 11:12:44 -0700

    Task solution

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,56 +1,16 @@
 #!/usr/bin/python3
-"""
-Script that lists all states from the database hbtn_0e_0_usa.
-"""
-
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-
-
-def list_states(username, password, database):
-    """
-    Lists all states from the database hbtn_0e_0_usa.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-
-    Returns:
-        None
-    """
-    try:
-        # Connect to MySQL server
-        db = MySQLdb.connect(host="localhost",
-                             port=3306,
-                             user=username,
-                             passwd=password,
-                             db=database)
-        #test
-        iCreate cursor object
-        cursor = db.cursor()
-
-        # Execute SQL query to fetch distinct states
-        cursor.execute("SELECT DISTINCT id, name FROM states ORDER BY id ASC")
-
-        # Fetch all rows
-        rows = cursor.fetchall()
-
-        # Display results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("MySQLdb Error:", e)
-
-    finally:
-        # Close cursor and database connection
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
+import sys
 
 
 if __name__ == "__main__":
-    username, password, database = sys.argv[1:]
-    list_states(username, password, database)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
